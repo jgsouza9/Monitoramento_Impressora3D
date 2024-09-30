122d017e2d47ff6f86f084c6a261300ced1690f1
João Gabriel de Souza Nogueira <141243105+jgsouza9@users.noreply.github.com>
2024-09-30 15:56:51 -0300

    Arquivo Vite atualizado

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
             input: ['resources/css/app.css', 
                 'resources/js/app.js',
                 'resources/css/cameraPage.css',
-                'resources/css/loginPage.css'
+                'resources/css/loginPage.css',
+                'resources/css/accessDenied.css'
             ],
             refresh: true,
         }),
