8baaa62304e63262bdd2fe76245de2ba29db46c8
João Gabriel de Souza Nogueira <141243105+jgsouza9@users.noreply.github.com>
2024-09-30 16:00:27 -0300

    Blade do Modal de Permissão negada

---
+++ resources/views/accessDenied.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    @vite('resources/css/accessDenied.css')
+</head>
+<body>
+    <div class="modal">
+        <button class="close-btn">&times;</button>
+        <h1>PERMISSÃO NEGADA</h1>
+        <p>o endereço IP utilizado não está cadastrado em nosso sistema.<br>Para acessar, por favor, verifique suas credenciais.</p>
+    </div>
+</body>
+</html>
